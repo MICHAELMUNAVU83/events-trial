f5bc39b22c7446d87bacca6fb3af2b5ebc05b86f
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-29 19:53:51 +0300

    Added put event

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Form from "./components/Form";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/events")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form data={data} />
+
+      <div className="container">
+
+      {data.map((event) => (
+        <div className="alldata" key={event.id}>
+          <h1>{event.name}</h1>
+          <img src={event.avatar_url} alt="avatar" />
+        </div>
+      ))}
+      </div>
     </div>
   );
 }
