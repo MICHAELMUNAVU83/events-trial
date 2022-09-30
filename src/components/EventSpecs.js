4f74137fcf4f6255d2a5387404622ccf98e3ade2
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-30 14:52:39 +0300

    Added

--- src/components/EventSpecs.js
+++ src/components/EventSpecs.js
@@ -5,50 +5,55 @@ function EventSpecs({ data, setData }) {
   const params = useParams();
   return (
     <div>
-      rctvbnm
       {data.map(
         (event) =>
           event.id === Number(params.id) && (
             <div>
               <h1>{event.name}</h1>
-              {event.tickets > 0 ? <h1>{event.tickets}</h1> : <h1>Sold out</h1>}
+              {event.tickets > 0 ? (
+                <div>
+                  {event.tickets}
 
-              <button
-                onClick={() => {
-                  if (event.tickets > 0) {
-                    fetch(`http://localhost:8002/events/${event.id}`, {
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      method: "PATCH",
-                      body: JSON.stringify({
-                        tickets: event.tickets - 1,
-                      }),
-                    })
-                      .then(function (response) {
-                        // console.log(response);
-                        return response.json();
-                      })
-                      .then(function (obj) {
-                        console.log(obj);
-                        if (event.tickets > 0) {
-                          setData((prevData) => {
-                            return prevData.map((item) => {
-                              if (item.id === event.id) {
-                                return obj;
-                              } else {
-                                return item;
-                              }
-                            });
+                  <button
+                    onClick={() => {
+                      if (event.tickets > 0) {
+                        fetch(`http://localhost:8002/events/${event.id}`, {
+                          headers: {
+                            "Content-Type": "application/json",
+                          },
+                          method: "PATCH",
+                          body: JSON.stringify({
+                            tickets: event.tickets - 1,
+                          }),
+                        })
+                          .then(function (response) {
+                            // console.log(response);
+                            return response.json();
+                          })
+                          .then(function (obj) {
+                            console.log(obj);
+                            if (event.tickets > 0) {
+                              setData((prevData) => {
+                                return prevData.map((item) => {
+                                  if (item.id === event.id) {
+                                    return obj;
+                                  } else {
+                                    return item;
+                                  }
+                                });
+                              });
+                            }
                           });
-                        }
-                      });
-                  }
-                }}
-              >
-                {" "}
-                add
-              </button>
+                      }
+                    }}
+                  >
+                    {" "}
+                    add
+                  </button>
+                </div>
+              ) : (
+                <h1>Sold out</h1>
+              )}
             </div>
           )
       )}
