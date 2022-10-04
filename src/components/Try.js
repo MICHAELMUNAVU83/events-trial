b7b1f6c5b3d709f247b70fe694328192da8cd9f3
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-04 06:48:53 +0300

    Finished styling log in form

--- src/components/Try.js
+++ src/components/Try.js
@@ -3,7 +3,25 @@ import React from "react";
 function Try() {
   return (
     <div>
-     
+      {/* <div className="form">
+        <input
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          type="text"
+          placeholder="username"
+        />
+        <input
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="text"
+          placeholder="password"
+        />
+        <button onClick={handleSubmit}>Login</button>
+      </div> */}
     </div>
   );
 }
