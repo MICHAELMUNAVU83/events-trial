7c4e6191173442249531a2d39e329322992e33e2
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-10-04 11:39:58 +0300

    Finished first styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -40,89 +40,16 @@ function Form({ setData, data }) {
       setData([...data, res.data]);
     });
   };
-  const form = (
-    <div classNameName="form">
-      <h1>Add an event</h1>
-      <input
-        value={eventName}
-        placeholder="Add a name"
-        onChange={(e) => {
-          setEventName(e.target.value);
-        }}
-        type="text"
-      />
-      <h1>Add a picture</h1>
 
-      <label htmlFor="file">
-        Select Image From Gallery
-        <input
-          type="file"
-          id="file"
-          onChange={(e) => {
-            uploadImage(e.target.files);
-          }}
-        />
-      </label>
-      <h1>Add tickets available</h1>
-
-      <input
-        value={tickets}
-        placeholder="Add the tickets availabe for this event"
-        onChange={(e) => {
-          setTickets(e.target.value);
-        }}
-        type="number"
-      />
-      <input
-        value={speakers}
-        placeholder="Add the speakers for this event"
-        onChange={(e) => {
-          setSpeakers(e.target.value);
-        }}
-        type="text"
-      />
-
-      <input
-        value={time}
-        onChange={(e) => {
-          setTime(e.target.value);
-        }}
-        type="string"
-        placeholder="Time the event starts"
-      />
-
-      <input
-        value={venue}
-        onChange={(e) => {
-          setVenue(e.target.value);
-        }}
-        type="string"
-        placeholder="Venue"
-      />
-      <input
-        value={date}
-        onChange={(e) => {
-          setDate(e.target.value);
-        }}
-        type="date"
-        placeholder="Date"
-      />
-      <input
-        type="event-description"
-        value={description}
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
-        placeholder="Event Description"
-      />
-    </div>
-  );
   return (
     <div className="signup-container">
       <div className="left-container">
         <h1>TECH ME OUT</h1>
         <div className="puppy">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/38816/image-from-rawpixel-id-542207-jpeg.png" />
+          <img
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/38816/image-from-rawpixel-id-542207-jpeg.png"
+            alt="logo"
+          />
         </div>
       </div>
       <div className="right-container">
@@ -135,111 +62,109 @@ function Form({ setData, data }) {
                 id="events-name"
                 placeholder="Event Name"
                 type="text"
+                value={eventName}
+                onChange={(e) => {
+                  setEventName(e.target.value);
+                }}
               ></input>
             </div>
             <div className="pets-photo">
               <button id="pets-upload">
                 <BiCamera />
+                <input
+                  type="file"
+                  id="file-selector"
+                  onChange={(e) => {
+                    uploadImage(e.target.files);
+                  }}
+                />
               </button>
               <label for="pets-upload">Upload a photo</label>
             </div>
           </div>
           <div className="set">
             <div className="pets-breed">
-              <label for="pets-breed">Breed</label>
+              <label for="events-venue">Venue</label>
               <input
-                id="pets-breed"
-                placeholder="Pet's breed"
+                id="events-venue"
+                placeholder="Venue..."
                 type="text"
+                value={venue}
+                onChange={(e) => {
+                  setVenue(e.target.value);
+                }}
               ></input>
             </div>
             <div className="pets-birthday">
-              <label for="pets-birthday">Birthday</label>
+              <label for="pets-birthday">Event Date</label>
               <input
                 id="pets-birthday"
-                placeholder="MM/DD/YYYY"
-                type="text"
+                type="date"
+                value={date}
+                onChange={(e) => {
+                  setDate(e.target.value);
+                }}
               ></input>
             </div>
           </div>
           <div className="set">
-            <div className="pets-gender">
-              <label for="pet-gender-female">Gender</label>
-              <div className="radio-container">
-                <input
-                  id="pet-gender-female"
-                  name="pet-gender"
-                  type="radio"
-                  value="female"
-                ></input>
-                <label for="pet-gender-female">Female</label>
-                <input
-                  id="pet-gender-male"
-                  name="pet-gender"
-                  type="radio"
-                  value="male"
-                ></input>
-                <label for="pet-gender-male">Male</label>
-              </div>
+            <div className="pets-breed">
+              <label for="events-venue">Tickets Available</label>
+              <input
+                id="events-tickets"
+                placeholder="Tickets Available..."
+                type="number"
+                value={tickets}
+                onChange={(e) => {
+                  setTickets(e.target.value);
+                }}
+              ></input>
             </div>
-            <div className="pets-spayed-neutered">
-              <label for="pet-spayed">Spayed or Neutered</label>
-              <div className="radio-container">
-                <input
-                  id="pet-spayed"
-                  name="spayed-neutered"
-                  type="radio"
-                  value="spayed"
-                ></input>
-                <label for="pet-spayed">Spayed</label>
-                <input
-                  id="pet-neutered"
-                  name="spayed-neutered"
-                  type="radio"
-                  value="neutered"
-                ></input>
-                <label for="pet-neutered">Neutered</label>
-              </div>
+            <div className="pets-birthday">
+              <label for="pets-birthday">Time the Event Starts</label>
+              <input
+                id="pets-birthday"
+                placeholder="Time..."
+                type="text"
+                value={time}
+                onChange={(e) => {
+                  setTime(e.target.value);
+                }}
+              ></input>
             </div>
           </div>
+
           <div className="pets-weight">
-            <label for="pet-weight-0-25">Weight</label>
+            <label for="pet-weight-0-25">Speakers at the event</label>
             <div className="radio-container">
               <input
-                id="pet-weight-0-25"
-                name="pet-weight"
-                type="radio"
-                value="0-25"
-              ></input>
-              <label for="pet-weight-0-25">0-25 lbs</label>
+                value={speakers}
+                placeholder="Add the speakers for this event"
+                onChange={(e) => {
+                  setSpeakers(e.target.value);
+                }}
+                type="text"
+              />
+            </div>
+            <label for="pet-weight-0-25">Event description</label>
+            <div className="radio-container">
               <input
-                id="pet-weight-25-50"
-                name="pet-weight"
-                type="radio"
-                value="25-50"
-              ></input>
-              <label for="pet-weight-25-50">25-50 lbs</label>
-              <input
-                id="pet-weight-50-100"
-                name="pet-weight"
-                type="radio"
-                value="50-100"
-              ></input>
-              <label for="pet-weight-50-100">50-100 lbs</label>
-              <input
-                id="pet-weight-100-plus"
-                name="pet-weight"
-                type="radio"
-                value="100+"
-              ></input>
-              <label for="pet-weight-100-plus">100+ lbs</label>
+                type="text"
+                value={description}
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+                placeholder="Event Description"
+              />
             </div>
           </div>
         </header>
         <footer>
           <div className="set">
             <button id="back">Back</button>
-            <button id="next">Next</button>
+            <button id="next" onClick={addEvent}>
+              Next
+            </button>
           </div>
         </footer>
       </div>
